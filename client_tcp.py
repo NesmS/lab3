12a6c5a00dca495c2d9ffb115e6bc25aff9900f6
NesmS <djct16@mail.ru>
2017-12-11 20:13:08 +0300

    Update client_tcp.py

--- client_tcp.py
+++ client_tcp.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 
 if __name__ == '__main__':
+    addr = input("server address: ")
     send_to = ("localhost", 9999)
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4, TCP
